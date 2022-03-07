7d5aa84c9ef79dde882683487988a9f987cb763a
LapisOnTheMoon <39343582+LapisOnTheMoon@users.noreply.github.com>
2022-03-07 16:32:08 -0500

    Update background.js
    Add context menu functionality

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,23 @@
 // Author: Alex Walczak (awal@awal.io)
+// Modified by: LapisOnTheMoon
+
+const CONTEXT_MENU_ID = 'duplicate_tab_ctx';
+const DUPLICATE_TAB_STR = 'Duplicate Tab';
+
+if (!chrome.contextMenus.onClicked.hasListeners()) {
+  chrome.contextMenus.create({
+    id: CONTEXT_MENU_ID,
+    type: 'normal',
+    title: DUPLICATE_TAB_STR,
+    contexts: ['all']
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === CONTEXT_MENU_ID) {
+      duplicateTab(tab);
+    }
+  });
+}
 
 // Duplicate tabs.Tab `tab`
 function duplicateTab(tab) {
